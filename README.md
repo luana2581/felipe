0cc51d5ce197f46fc1d6a6decfe83b2f06905e66
Felipe <159153862+luana2581@users.noreply.github.com>
2024-10-08 12:19:22 -0300

    08/10

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fleshcad
-# fleshcad
+a
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javaScript é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Russia?',
+    'A capital da Russia é Mouscou'
+)
+
+ciraCartao(
+    'matematica',
+    'Qual é o resultado de 9 x 9?',
+    'O resultado é 81'
+)
+
+criaCartao(
+    'Curiosidades inuteis',
+    'Precisa das 3 primeiras gerações para completar a pokedex?',
+    'Sim'
+)
